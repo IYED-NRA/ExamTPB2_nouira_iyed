1bd0353902d6184ec67e1b8154139377359661bd
Iyed Nouira <nouiraiyed2@gmail.com>
2025-07-09 19:52:56 +0100

    projet.py version final

--- projet.py
+++ projet.py
@@ -6,21 +6,21 @@ def produit (T):
 		return S
 
 Data=[1,3,5]
-<<<<<<< HEAD
+
 
 Prod=math.prod (Data)
 print ('le produit est :',Prod)
 
 
-=======
->>>>>>> a5f7041 (projet.py version 5)
+
+
 if Data:
 	print ('le produit est :',Prod(Data))
 	print ('le min est :',min(Data))
 	print ('le max est :',max(Data))
 else:
 print('dossier vide')
-<<<<<<< HEAD
 
-=======
+
+
 >>>>>>> a5f7041 (projet.py version 5)
